3da0865b4eff47ca058f1bf9c2c2c9051573eb0a
Conor Groenenberg <0965749@hr.nl>
2022-01-06 11:55:46 +0100

    put paramlib back

---
+++ Snerble.VRC.TouchControls/ParamLib/BaseParam.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MelonLoader;
+using UnityEngine;
+using VRC.SDK3.Avatars.ScriptableObjects;
+
+namespace ParamLib
+{
+    public class BaseParam
+    {
+        protected BaseParam(string paramName, VRCExpressionParameters.ValueType paramType)
+        {
+            ParamName = paramName;
+            _paramType = paramType;
+            ResetParam();
+        }
+
+        public void ResetParam() => (ParamIndex, ParameterLiteral) = ParamLib.FindParam(ParamName, _paramType);
+
+        public void ZeroParam() => ParamIndex = null;
+
+        public double ParamValue
+        {
+            get => _paramValue;
+            set
+            {
+                if (!ParamIndex.HasValue) return;
+                if (ParamLib.SetParameter(ParamIndex.Value, (float) value))
+                    _paramValue = value;
+            }
+        }
+
+        public int? ParamIndex;
+        
+        public readonly string ParamName;
+        private readonly VRCExpressionParameters.ValueType _paramType;
+        protected VRCExpressionParameters.Parameter ParameterLiteral;
+        private double _paramValue;
+    }
+
+    public class BoolBaseParam : BaseParam
+    {
+        public new bool ParamValue
+        {
+            get => Convert.ToBoolean(base.ParamValue);
+            set => base.ParamValue = Convert.ToDouble(value);
+        }
+        
+        public BoolBaseParam(string paramName) : base(paramName, VRCExpressionParameters.ValueType.Bool)
+        {
+        }
+    }
+
+    public class IntBaseParam : BaseParam
+    {
+        public new int ParamValue
+        {
+            get => (int) base.ParamValue;
+            set => base.ParamValue = value;
+        }
+        
+        public IntBaseParam(string paramName) : base(paramName, VRCExpressionParameters.ValueType.Int)
+        {
+        }
+    }
+    
+    public class FloatBaseParam : BaseParam
+    {
+        private static readonly List<FloatBaseParam> PrioritisedParams = new List<FloatBaseParam>();
+        private readonly bool _wantsPriority;
+        
+        public new float ParamValue
+        {
+            get => (float) base.ParamValue;
+            set => base.ParamValue = value;
+        }
+        
+        public FloatBaseParam(string paramName, bool prioritised = false) : base(paramName, VRCExpressionParameters.ValueType.Float)
+        {
+            _wantsPriority = prioritised;
+            if (_wantsPriority)
+                MelonCoroutines.Start(KeepParamPrioritised());
+        }
+
+        public new void ResetParam()
+        {
+            base.ResetParam();
+
+            // If we found a parameter literal, and this param need priority, and it's one of the first 8 params
+            if (!ParamIndex.HasValue || !_wantsPriority) return;    // Check if we have a value since sometimes people don't use both x and y for XY params
+            
+            // Check if this parameter has an index lower than any of the prioritised params, and if so, replace the parameter it's lower than
+            if (PrioritisedParams.Count < 8) // If we have less than 8 params, we can just add it to the end
+                PrioritisedParams.Add(this);
+            else
+                foreach (var param in PrioritisedParams.Where(param => param.ParamIndex.HasValue && ParamIndex.Value < param.ParamIndex.Value))
+                {
+                    // Prioritise this param
+                    PrioritisedParams.Remove(param);
+                    PrioritisedParams.Add(this);
+                    return;
+                }
+        }
+
+        public new void ZeroParam()
+        {
+            base.ZeroParam();
+
+            if (PrioritisedParams.Contains(this))
+                PrioritisedParams.Remove(this);
+        }
+        
+        private IEnumerator KeepParamPrioritised()
+        {
+            for (;;)
+            {
+                yield return new WaitForSeconds(5);
+                if (!PrioritisedParams.Contains(this) || !ParamIndex.HasValue) continue;
+                ParamLib.PrioritizeParameter(ParamIndex.Value);
+            }
+        }
+    }
+}
---
+++ Snerble.VRC.TouchControls/ParamLib/ParamLib.cs
@@ -0,0 +1,82 @@
+using System.Linq;
+using UnhollowerRuntimeLib.XrefScans;
+using VRC.SDK3.Avatars.ScriptableObjects;
+using MethodInfo = System.Reflection.MethodInfo;
+
+// ReSharper disable Unity.NoNullPropagation        Rider please, we both know this wasn't gonna be pretty...
+
+namespace ParamLib
+{
+    public static class ParamLib
+    {
+        private static AvatarPlayableController LocalPlayableController => LocalAnimParamController
+            ?.field_Private_AvatarPlayableController_0;
+
+        private static AvatarAnimParamController LocalAnimParamController => VRCPlayer.field_Internal_Static_VRCPlayer_0
+            ?.field_Private_AnimatorControllerManager_0?.field_Private_AvatarAnimParamController_0;
+        
+        private static readonly MethodInfo PrioritizeMethod = typeof(AvatarPlayableController).GetMethods().Where(info =>
+                info.Name.Contains("Method") && !info.Name.Contains("PDM") && info.Name.Contains("Public")
+                && info.GetParameters().Length == 1 && info.Name.Contains("Int32") &&
+                info.ReturnType == typeof(void))
+            .First(method => XrefScanner.UsedBy(method).Any(xrefs =>
+                XrefScanner.UsedBy(xrefs.TryResolve()).Any(i => i.TryResolve().Name == "OnEnable")));
+
+        private static readonly MethodInfo SetMethod = typeof(AvatarPlayableController).GetMethods().First(m =>
+            m.Name.Contains("Boolean_Int32_Single") && !m.Name.Contains("PDM") &&
+            XrefScanner.UsedBy(m).All(inst => inst.Type == XrefType.Method && inst.TryResolve()?.DeclaringType == typeof(AvatarPlayableController)));
+
+        public static void PrioritizeParameter(int paramIndex)
+        {
+            if (LocalPlayableController == null) return;
+
+            PrioritizeMethod.Invoke(LocalPlayableController, new object[] { paramIndex });
+        }
+
+        public static VRCExpressionParameters.Parameter[] GetLocalParams() =>
+            GetParams(VRCPlayer.field_Internal_Static_VRCPlayer_0);
+
+        public static VRCExpressionParameters.Parameter[] GetParams(VRCPlayer player) => player?.prop_VRCAvatarManager_0
+            ?.prop_VRCAvatarDescriptor_0?.expressionParameters
+            ?.parameters;
+
+        public static bool DoesParamExist(string paramName, VRCExpressionParameters.ValueType paramType,
+            VRCExpressionParameters.Parameter[] parameters = null)
+        {
+            // If they're null, then try getting LocalParams
+            parameters = parameters ?? GetLocalParams();
+            
+            // Separate Length from nulll check, otherwise you'll get a null exception if parameters are null
+            return parameters != null && parameters.Any(p => p.name == paramName && p.valueType == paramType);
+        }
+        
+        public static (int?, VRCExpressionParameters.Parameter) FindParam(string paramName, VRCExpressionParameters.ValueType paramType,
+            VRCExpressionParameters.Parameter[] parameters = null)
+        {
+            // If they're null, then try getting LocalParams
+            parameters = parameters ?? GetLocalParams();
+            
+            if (parameters == null) return (null, null);
+            
+
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                var param = parameters[i];
+                if (param.name == null) continue;
+                if (param.name == paramName && param.valueType == paramType) return (i, parameters[i]);
+            }
+
+            return (null, null);
+        }
+        
+        public static double? GetParamDefaultValue(VRCExpressionParameters.Parameter param) => param?.defaultValue;
+
+        public static bool SetParameter(int paramIndex, float value)
+        {
+            if (LocalAnimParamController?.field_Private_AvatarPlayableController_0 == null) return false;
+
+            SetMethod.Invoke(LocalAnimParamController.field_Private_AvatarPlayableController_0, new object[] {paramIndex, value});
+            return true;
+        }
+    }
+}
